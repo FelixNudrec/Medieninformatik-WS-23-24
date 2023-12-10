20947609974cbb5d39c0c805904d432a3de79354
Felix Wahler <62222775+FelixNudrec@users.noreply.github.com>
2023-12-10 10:40:51 +0100

    Beginn

---
+++ mein_uni_tag/android/app/src/main/kotlin/com/example/mein_uni_tag/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mein_uni_tag
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mein_uni_tag/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_analog_clock/flutter_analog_clock.dart';
+import 'package:intl/intl.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Image.network(
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Logo_TU_Dresden_small.svg/1200px-Logo_TU_Dresden_small.svg.png',
+            width: 30,
+            height: 30,
+          ),
+          title: Text('Mein Uni-Tag', style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.menu, color: Colors.white),
+              onPressed: () {
+                // Logik für das Öffnen des Menüs hinzufügen
+              },
+            ),
+          ],
+          backgroundColor: Colors.indigo[900],
+        ),
+        body: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerRight,
+              child: const AnalogClock(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    '${_getFormattedWeekday()}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  SizedBox(width: 8),
+                  Text('|', style: TextStyle(fontSize: 16)),
+                  SizedBox(width: 8),
+                  Text(
+                    '${_getFormattedDate()}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _getFormattedWeekday() {
+    return DateTime.now().toLocal().weekday.toString();
+  }
+
+  String _getFormattedDate() {
+    return DateTime.now().toLocal().toString().split(' ')[0];
+  }
+}
