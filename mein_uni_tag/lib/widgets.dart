dc813f3af56fff1a2f83b8bf7e1083b5246fe7d0
Hahnnibal <153361531+Hahnnibal@users.noreply.github.com>
2023-12-15 16:15:59 +0100

    Update widgets.dart

--- mein_uni_tag/lib/widgets.dart
+++ mein_uni_tag/lib/widgets.dart
@@ -31,9 +31,9 @@ class _SubjectScheduleWidgetState extends State<SubjectScheduleWidget> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                _buildDateTimeWidget('Vorlesung: ${_formatDateTime(widget.vorlesungDateTime)}', isVorlesungToday, Icons.school),
+                _buildDateTimeWidget(' ${_formatDateTime(widget.vorlesungDateTime)}', isVorlesungToday, Icons.school),
                 SizedBox(height: 3), // Space between Vorlesung and Übung
-                _buildDateTimeWidget('Übung: ${_formatDateTime(widget.ubungDateTime)}', isUbungToday, Icons.edit),
+                _buildDateTimeWidget(' ${_formatDateTime(widget.ubungDateTime)}', isUbungToday, Icons.edit),
               ],
             ),
           ),
